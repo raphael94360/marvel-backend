6a7e6c56be509f8832ec1a9bf50f70b3fbe025a4
raphael94360 <paulberthet94@gmail.com>
2022-08-26 10:30:42 +0200

    route /characters

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require("express")
 const cors = require("cors")
 const axios = require("axios")
 
+const apiKey = process.env.apiKey
+
 const app = express()
 app.use(cors())
-
-const apiKey = process.env.apiKey
 
 console.log(apiKey)
 
@@ -14,9 +14,24 @@ app.get("/", (req, res) => {
   res.json({ message: "mon backend marvel" })
 })
 
+app.get("/characters", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://raphael-marvel-backend.herokuapp.com/characters?apiKey=" + apiKey
+      // "http://localhost:4000/characters?apiKey=" + apiKey
+    )
+    res.status(200).json(response.data)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
+
 app.get("/comics", async (req, res) => {
   try {
-    const response = await axios.get(`https://raphael-marvel-backend.herokuapp.com/comics?apiKey=${apiKey}`)
+    const response = await axios.get(
+      `http://localhost:4000/comics?apiKey=${apiKey}&limit=${req.query.limit}`
+      // `https://raphael-marvel-backend.herokuapp.com/comics?apiKey=${apiKey}&limit=${req.query.limit}`
+    )
 
     res.status(200).json(response.data)
   } catch (error) {
